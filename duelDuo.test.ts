39f471ff19950fcb118b6a41c009b1f274eaeca8
Tomas Fisher <dontomassino777@gmail.com>
2021-11-19 12:27:24 -0700

    part 4 done, assessment complete

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,18 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+
+test('Draw button displays choices div', async () => {
+    const button = await driver.findElement(By.id('draw')).click()
+    const div = await driver.findElement(By.id('choices'))
+    const divDisplayed = await div.isDisplayed()
+    expect(divDisplayed).toBe(true)
+})
+
+test('Add to Duo button displays players div', async () => {
+    const duobutton = await driver.findElement(By.xpath("//*[contains(text(), 'add')]")).click()
+    const playersDiv = await driver.findElement(By.id('player-id'))
+    const playersDivDisplayed = await playersDiv.isDisplayed()
+    expect(playersDivDisplayed).toBe(true)
+})
