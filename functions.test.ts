8472c47bb950b5d13e2f0bffcd6e084ae6535795
Tomas Fisher <dontomassino777@gmail.com>
2021-11-19 10:58:20 -0700

    part 3 done

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,10 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test("shuffle the array", () => {
+        expect(shuffleArray([1,2,3,4,5])).not.toEqual([1,2,3,4,5])
+    })
+    test("return array the same length as original array", () => {
+        expect(shuffleArray([1,2,3,4,5])).toHaveLength(5)
+    })
 })
